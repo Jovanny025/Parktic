396ed82168c050b6c91a432165d0809fcb7b9619
Jovanny valencia <jovanny.valencia@est.iudigital.edu.co>
2022-09-13 21:51:38 -0500

    mi quinto commit

---
+++ Pages/Camiones.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace parktic.App.Pages
+{
+    public class CamionesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
